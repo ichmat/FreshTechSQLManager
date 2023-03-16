b4fb9c3ee6b014209a2406a1abe5a49d95430df3
Mattéo LOPES <matteo.lopes@ynov.com>
2023-03-16 09:39:30 +0100

    Ajoutez des fichiers projet.

---
+++ FreshTechSQLManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FreshTechSQLManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
